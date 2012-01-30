53952b7b2bb135e06c3214c162c20dc781ca9b25
Wiz <wiz@haxx.es>
2012-01-30 09:33:25 +0100

    First shot at circular buffer

--- src/circularbuffer.hh
+++ src/circularbuffer.hh
@@ -6,17 +6,47 @@
 namespace nyanstream
 {
 
+template<class T>
 class CircularBuffer
 {
 private:
-    int size;
-    uint8_t* data;
+    size_t size;
+    T* data;
+    size_t positionWrite, positionRead;
 
 public:
-    CircularBuffer(int size);
-    ~CircularBuffer();
+    CircularBuffer(size_t size)
+    {
+        resize(newSize);
+    }
 
-    
+    ~CircularBuffer()
+    {
+        delete[] data;
+    }
+
+    void resize(size_t size)
+    {
+        size = newSize;
+        data = new uint8_t[size];
+        positionWrite = positionRead = 0;
+    }
+
+    CircularBuffer& operator<<(T v)
+    {
+        data[positionWrite] = v;
+        positionWrite = (positionWrite + 1) % size;
+
+        return *this;
+    }
+
+    CircularBuffer& operator>>(T& v)
+    {
+        v = data[positionRead];
+        positionRead = (positionWrite + 1) % size;
+
+        return *this;
+    }
 };
 
 }
